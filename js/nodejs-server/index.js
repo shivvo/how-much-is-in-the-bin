2c46f9fb36c8a0907d34404aa6fe3896789cbba0
vagrant <vagrant@vagrant.vm>
2018-02-21 04:36:54 +0000

    Finish message server

--- js/nodejs-server/index.js
+++ js/nodejs-server/index.js
@@ -1,13 +1,42 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var database = [];
+
+function createMessage(sender, content) {
+  return {
+    sender: sender,
+    content: content,
+    timestamp: Date()
+  };
+}
+
 var app = express();
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+  res.send('hello, world!\n');
 });
 
-var server = app.listen(8081, function () {
+app.post('/sendMessage', function (req, res) {
+  var sender = req.body.sender;
+  var content = req.body.content;
+  var message = createMessage(sender, content);
+  database.push(message);
+  res.send("");
+});
+
+app.get('/allMessages', function (req, res) {
+  var formattedMessages = database.map(function (e) {
+    return e.sender + "(" + e.timestamp + "): " + e.content;
+  });
+  var allMessagesString = formattedMessages.join("\n") + "\n";
+  res.send(allMessagesString);
+});
+
+var server = app.listen(20000, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Chatroom app listening at http://%s:%s", host, port)
 });
